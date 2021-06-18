8624a3634dd92b656ad6b5815e8e11a29e71164e
Super <mmatt2340@gmail.com>
2021-06-18 07:25:08 -0400

    Search bar for offline menu pog

--- source/onlinemod/OfflineMenuState.hx
+++ source/onlinemod/OfflineMenuState.hx
@@ -3,9 +3,11 @@ package onlinemod;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.addons.ui.FlxUIButton;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
+import flixel.util.FlxStringUtil;
+import flixel.addons.ui.FlxUIButton;
+import flixel.addons.ui.FlxInputText;
 
 import sys.io.File;
 import sys.FileSystem;
@@ -15,9 +17,12 @@ using StringTools;
 class OfflineMenuState extends MusicBeatState
 {
   var curSelected:Int = 0;
+  var searchField:FlxInputText;
+  var searchButton:FlxUIButton;
 
   var songs:Array<String> = [];
   var grpSongs:FlxTypedGroup<Alphabet>;
+  var dataDir:String = "assets/onlinedata/data/";
   public static var optionsButton:FlxUIButton;
 
   override function create()
@@ -25,36 +30,23 @@ class OfflineMenuState extends MusicBeatState
     var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image("menuDesat"));
     bg.color = 0xFFFF6E6E;
     add(bg);
+    searchField = new FlxInputText(10, 100, 1152, 20);
+    searchField.maxLength = 81;
+    add(searchField);
+
+    searchButton = new FlxUIButton(10 + 1152 + 9, 100, "Find", () -> {
+      refreshList(true,searchField.text);
+      searchField.hasFocus = false;
+    });
+    searchButton.setLabelFormat(24, FlxColor.BLACK, CENTER);
+    searchButton.resize(100, searchField.height);
+    add(searchButton);
 
 
-    grpSongs = new FlxTypedGroup<Alphabet>();
-    add(grpSongs);
 
-    var i:Int = 0;
-    var dataDir:String = "assets/onlinedata/data/";
-    if (FileSystem.exists(dataDir))
-    {
-      for (directory in FileSystem.readDirectory(dataDir))
-      {
-        for (file in FileSystem.readDirectory(dataDir + directory))
-        {
-          if (StringTools.endsWith(file, '.json'))
-          {
-            songs.push(dataDir + directory + "/" + file);
+    
 
-            var controlLabel:Alphabet = new Alphabet(0, (70 * i) + 30, file.substr(0, file.length - 5), true, false);
-      			controlLabel.isMenuItem = true;
-      			controlLabel.targetY = i;
-            if (i != 0)
-              controlLabel.alpha = 0.6;
-      			grpSongs.add(controlLabel);
-
-            i++;
-          }
-        }
-      }
-    }
-
+    refreshList();
 
     FlxG.mouse.visible = true;
     FlxG.autoPause = true;
@@ -63,7 +55,6 @@ class OfflineMenuState extends MusicBeatState
     super.create();
 
     optionsButton = new FlxUIButton(1100, 40, "Options", () -> {
-
       FlxG.mouse.visible = false;
       FlxG.switchState(new OptionsMenu());
     });
@@ -71,7 +62,36 @@ class OfflineMenuState extends MusicBeatState
     optionsButton.resize(150, 30);
     add(optionsButton);
   }
+  function refreshList(?reload=false,?search = ""){
+    curSelected = 0;
+    if(reload){grpSongs.destroy();}
+    grpSongs = new FlxTypedGroup<Alphabet>();
+    add(grpSongs);
+    songs = [];
+    var i:Int = 0;
+    if (FileSystem.exists(dataDir))
+    {
+      for (directory in FileSystem.readDirectory(dataDir))
+      {
+        for (file in FileSystem.readDirectory(dataDir + directory))
+        {
+          if (StringTools.endsWith(file, '.json') && (search == "" || FlxStringUtil.contains(file.toLowerCase(),search.toLowerCase()))) // Handles searching
+          {
+            songs.push(dataDir + directory + "/" + file);
 
+            var controlLabel:Alphabet = new Alphabet(0, (70 * i) + 30, file.substr(0, file.length - 5), true, false);
+            controlLabel.isMenuItem = true;
+            controlLabel.targetY = i;
+            if (i != 0)
+              controlLabel.alpha = 0.6;
+            grpSongs.add(controlLabel);
+
+            i++;
+          }
+        }
+      }
+    }
+  }
   override function update(elapsed:Float)
   {
     super.update(elapsed);
@@ -81,10 +101,8 @@ class OfflineMenuState extends MusicBeatState
       FlxG.switchState(new OnlineMenuState());
     }
 
-    if (controls.UP_P)
-      changeSelection(-1);
-    if (controls.DOWN_P)
-      changeSelection(1);
+      if (controls.UP_P && FlxG.keys.pressed.SHIFT){changeSelection(-5);} else if (controls.UP_P){changeSelection(-1);}
+      if (controls.DOWN_P && FlxG.keys.pressed.SHIFT){changeSelection(5);} else if (controls.DOWN_P){changeSelection(1);}
 
     if (controls.ACCEPT && songs.length > 0)
     {
