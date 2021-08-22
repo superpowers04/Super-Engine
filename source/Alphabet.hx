39c9efbff7aab1c5e24357836aa721f76676b550
Super <mmatt2340@gmail.com>
2021-08-22 15:07:26 -0400

    Move Alphabet to the left a bit

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -226,7 +226,7 @@ class Alphabet extends FlxSpriteGroup
 			var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
 
 			y = FlxMath.lerp(y, (scaledY * 120) + (FlxG.height * 0.48), 0.30);
-			x = FlxMath.lerp(x, (targetY * 20) + 90, 0.30);
+			x = FlxMath.lerp(x, (targetY * 20) + 70, 0.30);
 		}
 
 		super.update(elapsed);
