cb4b3b023c8357e6f66c3839f6afbf65b3e9a944
Super <mmatt2340@gmail.com>
2021-09-09 18:10:41 -0400

    Disable highscore shit

--- source/Highscore.hx
+++ source/Highscore.hx
@@ -13,20 +13,20 @@ class Highscore
 
 	public static function saveScore(song:String, score:Int = 0, ?diff:Int = 0):Void
 	{
-		var daSong:String = formatSong(song, diff);
+		// var daSong:String = formatSong(song, diff);
 
 
 
-		if(!FlxG.save.data.botplay)
-		{
-			if (songScores.exists(daSong))
-			{
-				if (songScores.get(daSong) < score)
-					setScore(daSong, score);
-			}
-			else
-				setScore(daSong, score);
-		}else trace('BotPlay detected. Score saving is disabled.');
+		// if(!FlxG.save.data.botplay)
+		// {
+		// 	if (songScores.exists(daSong))
+		// 	{
+		// 		if (songScores.get(daSong) < score)
+		// 			setScore(daSong, score);
+		// 	}
+		// 	else
+		// 		setScore(daSong, score);
+		// }else trace('BotPlay detected. Score saving is disabled.');
 	}
 
 	public static function saveWeekScore(week:Int = 1, score:Int = 0, ?diff:Int = 0):Void
@@ -34,18 +34,18 @@ class Highscore
 
 
 
-		if(!FlxG.save.data.botplay)
-		{
-			var daWeek:String = formatSong('week' + week, diff);
+		// if(!FlxG.save.data.botplay)
+		// {
+		// 	var daWeek:String = formatSong('week' + week, diff);
 
-			if (songScores.exists(daWeek))
-			{
-				if (songScores.get(daWeek) < score)
-					setScore(daWeek, score);
-			}
-			else
-				setScore(daWeek, score);
-		}else trace('BotPlay detected. Score saving is disabled.');
+		// 	if (songScores.exists(daWeek))
+		// 	{
+		// 		if (songScores.get(daWeek) < score)
+		// 			setScore(daWeek, score);
+		// 	}
+		// 	else
+		// 		setScore(daWeek, score);
+		// }else trace('BotPlay detected. Score saving is disabled.');
 	}
 
 	/**
@@ -53,10 +53,10 @@ class Highscore
 	 */
 	static function setScore(song:String, score:Int):Void
 	{
-		// Reminder that I don't need to format this song, it should come formatted!
-		songScores.set(song, score);
-		FlxG.save.data.songScores = songScores;
-		FlxG.save.flush();
+		// // Reminder that I don't need to format this song, it should come formatted!
+		// songScores.set(song, score);
+		// FlxG.save.data.songScores = songScores;
+		// FlxG.save.flush();
 	}
 
 	public static function formatSong(song:String, diff:Int):String
@@ -89,9 +89,9 @@ class Highscore
 
 	public static function load():Void
 	{
-		if (FlxG.save.data.songScores != null)
-		{
-			songScores = FlxG.save.data.songScores;
-		}
+		// if (FlxG.save.data.songScores != null)
+		// {
+		// 	songScores = FlxG.save.data.songScores;
+		// }
 	}
 }
