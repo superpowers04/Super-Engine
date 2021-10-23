e03b69c661647ad293516d2834fda512f97fc314
Super <mmatt2340@gmail.com>
2021-10-23 19:14:15 -0400

    Lightspeed, streaks by BF

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -28,7 +28,7 @@ using StringTools;
 
 class MainMenuState extends SickMenuState
 {
-	public static var ver:String = "0.6.3";
+	public static var ver:String = "0.7.0";
 	
 	public static var firstStart:Bool = true;
 
