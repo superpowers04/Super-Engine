d5a9e18f5f669b04a231f6d4886413364004615d
Super <mmatt2340@gmail.com>
2022-03-20 09:12:49 -0400

    Killing sustain notes when created actually works

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1883,6 +1883,7 @@ class PlayState extends MusicBeatState
 					oldNote = unspawnNotes[Std.int(unspawnNotes.length - 1)];
 
 					var sustainNote:Note = new Note(daStrumTime + (Conductor.stepCrochet * susNote) + Conductor.stepCrochet, daNoteData, oldNote, true,null,songNotes[3],songNotes,gottaHitNote);
+					if(sustainNote.killNote){sustainNote.destroy();continue;}
 					sustainNote.scrollFactor.set();
 					sustainNote.sustainLength = susLength;
 					unspawnNotes.push(sustainNote);
