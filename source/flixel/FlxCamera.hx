9e12263a0d2150d4e8bf63532e86138bde2b3351
Super <38338700+superpowers04@users.noreply.github.com>
2025-04-14 22:15:11 -0400

    I didn't steal a fix from Psych Engine, I swear, you haven o proof

--- source/flixel/FlxCamera.hx
+++ source/flixel/FlxCamera.hx
@@ -1139,7 +1139,7 @@ class FlxCamera extends FlxBasic
 		// follow the target, if there is one
 		if (target != null)
 		{
-			updateFollow();
+			updateFollowDelta(elapsed);
 		}
 
 		updateScroll();
@@ -1152,6 +1152,93 @@ class FlxCamera extends FlxBasic
 		updateShake(elapsed);
 	}
 
+	public function updateFollowDelta(?elapsed:Float = 0):Void
+	{
+		// Either follow the object closely,
+		// or double check our deadzone and update accordingly.
+		if (deadzone == null)
+		{
+			target.getMidpoint(_point);
+			_point.addPoint(targetOffset);
+			_scrollTarget.set(_point.x - width * 0.5, _point.y - height * 0.5);
+		}
+		else
+		{
+			var edge:Float;
+			var targetX:Float = target.x + targetOffset.x;
+			var targetY:Float = target.y + targetOffset.y;
+
+			if (style == SCREEN_BY_SCREEN)
+			{
+				if (targetX >= viewRight)
+				{
+					_scrollTarget.x += viewWidth;
+				}
+				else if (targetX + target.width < viewLeft)
+				{
+					_scrollTarget.x -= viewWidth;
+				}
+
+				if (targetY >= viewBottom)
+				{
+					_scrollTarget.y += viewHeight;
+				}
+				else if (targetY + target.height < viewTop)
+				{
+					_scrollTarget.y -= viewHeight;
+				}
+				
+				// without this we see weird behavior when switching to SCREEN_BY_SCREEN at arbitrary scroll positions
+				bindScrollPos(_scrollTarget);
+			}
+			else
+			{
+				edge = targetX - deadzone.x;
+				if (_scrollTarget.x > edge)
+				{
+					_scrollTarget.x = edge;
+				}
+				edge = targetX + target.width - deadzone.x - deadzone.width;
+				if (_scrollTarget.x < edge)
+				{
+					_scrollTarget.x = edge;
+				}
+
+				edge = targetY - deadzone.y;
+				if (_scrollTarget.y > edge)
+				{
+					_scrollTarget.y = edge;
+				}
+				edge = targetY + target.height - deadzone.y - deadzone.height;
+				if (_scrollTarget.y < edge)
+				{
+					_scrollTarget.y = edge;
+				}
+			}
+
+			if ((target is FlxSprite))
+			{
+				if (_lastTargetPosition == null)
+				{
+					_lastTargetPosition = FlxPoint.get(target.x, target.y); // Creates this point.
+				}
+				_scrollTarget.x += (target.x - _lastTargetPosition.x) * followLead.x;
+				_scrollTarget.y += (target.y - _lastTargetPosition.y) * followLead.y;
+
+				_lastTargetPosition.x = target.x;
+				_lastTargetPosition.y = target.y;
+			}
+		}
+
+		var mult:Float = 1 - Math.exp(-elapsed * followLerp / (1/60));
+		scroll.x += (_scrollTarget.x - scroll.x) * mult;
+		scroll.y += (_scrollTarget.y - scroll.y) * mult;
+	}
+
+	public function set_followLerp(value:Float)
+	{
+		return followLerp = value;
+	}
 	/**
 	 * Updates (bounds) the camera scroll.
 	 * Called every frame by camera's `update()` method.
@@ -1887,11 +1974,6 @@ class FlxCamera extends FlxBasic
 			&& (rect.bottom > viewMarginTop) && (rect.y < viewMarginBottom);
 		rect.putWeak();
 		return contained;
-	}
-
-	function set_followLerp(Value:Float):Float
-	{
-		return followLerp = FlxMath.bound(Value, 0, 60 / FlxG.updateFramerate);
 	}
 
 	function set_width(Value:Int):Int
