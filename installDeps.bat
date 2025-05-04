6e201800629770426a434c14da6c5e443b30a0c8
Super <38338700+superpowers04@users.noreply.github.com>
2025-05-04 03:26:43 -0400

    Oop linc_discord-rpc, not discord_rpc

--- installDeps.bat
+++ installDeps.bat
@@ -7,6 +7,6 @@ haxelib run lime setup
 haxelib install flixel-tools
 haxelib install flixel-ui 2.6.3
 haxelib install flixel-addons 3.2.3
-haxelib git discord_rpc https://github.com/superpowers04/linc_discord-rpc
+haxelib git linc_discord-rpc https://github.com/superpowers04/linc_discord-rpc
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
